91080ab32bdc30817a7fc0da9cb28c00ac2eef4e
jchicago98 <104684466+jchicago98@users.noreply.github.com>
2022-11-13 21:16:36 -0600

    Added blog page component, future updates to follow

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { BlogPageComponent } from './blog-page/blog-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    BlogPageComponent
   ],
   imports: [
     BrowserModule,
