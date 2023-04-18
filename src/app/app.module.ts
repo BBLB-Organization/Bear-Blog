44af0890a66c6a9d70a5ff464a32c257f99ae533
Jorge Morin <jmorin1@hawk.iit.edu>
2023-04-18 14:16:12 -0500

    Showing blog page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { BlogPageComponent } from './blog-page/blog-page.component';
+import { BlogPageComponent } from './pages/blog-page/blog-page.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent,
     BlogPageComponent
   ],
   imports: [
