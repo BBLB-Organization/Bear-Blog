81398203e2270e0c05edfd11a517f5c46f9319c2
Jorge Morin <jmorin1@hawk.iit.edu>
2023-04-21 16:55:03 -0500

    Have new endpoints for bear blog

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  blogURL: '',
-  imageURL: ''
+  blogURL: 'https://bear-blog-api-production.up.railway.app/blog',
+  imageURL: 'https://bear-blog-api-production.up.railway.app/image'
 };
